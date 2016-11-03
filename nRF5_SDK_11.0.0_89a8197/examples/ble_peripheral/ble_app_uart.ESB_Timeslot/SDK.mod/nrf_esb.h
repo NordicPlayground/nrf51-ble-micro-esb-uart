792a102fc7e186432902f4ac3f2adb18f5b93570
P.K.Chan <pak.kee.chan@nordicsemi.no>
2016-11-03 17:25:51 +0800

    ESB to use SWI3 instead of SWI0, because SWI0 is used by app_timer.

--- nRF5_SDK_11.0.0_89a8197/examples/ble_peripheral/ble_app_uart.ESB_Timeslot/SDK.mod/nrf_esb.h
+++ nRF5_SDK_11.0.0_89a8197/examples/ble_peripheral/ble_app_uart.ESB_Timeslot/SDK.mod/nrf_esb.h
@@ -71,11 +71,11 @@ STATIC_ASSERT(NRF_ESB_MAX_PAYLOAD_LENGTH <= 252);
 #define     NRF_ESB_CRC_RESET_VALUE             0xFFFF              /**< CRC reset value*/
 
 #ifdef NRF51
-#define ESB_EVT_IRQ        SWI0_IRQn                                /**< ESB Event IRQ number when running on a nRF51 device. */
-#define ESB_EVT_IRQHandler SWI0_IRQHandler                          /**< The handler for ESB_EVT_IRQ when running on a nRF51 device. */
+#define ESB_EVT_IRQ        SWI3_IRQn                                /**< ESB Event IRQ number when running on a nRF51 device. */
+#define ESB_EVT_IRQHandler SWI3_IRQHandler                          /**< The handler for ESB_EVT_IRQ when running on a nRF51 device. */
 #elif defined (NRF52)
-#define ESB_EVT_IRQ        SWI0_EGU0_IRQn                           /**< ESB Event IRQ number when running on a nRF52 device. */
-#define ESB_EVT_IRQHandler SWI0_EGU0_IRQHandler                     /**< The handler for ESB_EVT_IRQ when running on a nRF52 device. */
+#define ESB_EVT_IRQ        SWI3_EGU3_IRQn                           /**< ESB Event IRQ number when running on a nRF52 device. */
+#define ESB_EVT_IRQHandler SWI3_EGU3_IRQHandler                     /**< The handler for ESB_EVT_IRQ when running on a nRF52 device. */
 #endif /* NRF51 */
 
 #define     NRF_ESB_SYS_TIMER                   NRF_TIMER2          /**< System timer used by nrf_esb */
